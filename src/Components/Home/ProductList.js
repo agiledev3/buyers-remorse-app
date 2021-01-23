4d9ed56f9d64243c492752e5465e20e57a26ae5e
Dmytro Kulak <mail@dmytrokulak.net>
2021-01-23 19:20:38 +0200

    ProductList: add caption

--- src/Components/Home/ProductList.js
+++ src/Components/Home/ProductList.js
@@ -5,6 +5,10 @@ import { Link } from 'react-router-dom'
 function ProductList(props) {
     return (
         <div style={containerStyle}>
+            <div>
+                <div style={captionStyle}>Buyer's regret <i className="far fa-user-circle" style={userIconStyle}></i></div>
+                <div style={captionStyle}>My wishlist</div>
+            </div>
             {props.allProducts.map((product) => (
                 <ListItem key={product.id} product={product}/>
             ))}
@@ -20,7 +24,14 @@ const containerStyle = {
     minWidth:'15rem',
     minHeight:'20rem', 
     backgroundColor:"white",
-    position: 'relative'
+    position: 'relative',
+    color: 'black',
+}
+
+const captionStyle ={
+    border: '1px solid black',
+    padding: '0.3rem 0',
+    textAlign: 'center'
 }
 
 const actionBtnStyle = {
@@ -28,11 +39,16 @@ const actionBtnStyle = {
     padding: '0.1rem',
     cursor: "pointer",
     margin: "0.1rem 0.1rem",
-    color: 'black',
     fontSize: '2rem',
     position: 'absolute',
     right: '0',
     bottom:'0'
 }
+
+const userIconStyle = {
+    float:'right', 
+    padding: '0.5rem 0.5rem 0.5rem 0'
+}
+
 
 export default ProductList

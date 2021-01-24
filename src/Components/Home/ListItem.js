2bf0691c5c118e3945ad0d805ab976c2d8be1403
Dmytro Kulak <mail@dmytrokulak.net>
2021-01-24 21:57:34 +0200

    Refactor to use bootstrap

--- src/Components/Home/ListItem.js
+++ src/Components/Home/ListItem.js
@@ -1,59 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Card, Col, Row, Button, Badge } from 'react-bootstrap'
 
 function ListItem(prop) {
-    const {id, name, daysLeft, likeCount} = prop.product;
+    const { id, name, daysLeft, likeCount } = prop.product;
 
     return (
-        <div style={boxStyle}>
-            <div style={captionStyle}>
-                {name}
-            </div>
-            <div style={contentStyle}>
-                <span>
+        <Card className="p-3">
+             <Card.Title>{name}</Card.Title>
+             <Row>
+                <Col>
                     <i className="far fa-clock"></i> {daysLeft} days left
-                </span>
-                <span>
-                    {/* ToDo:: remove sup after Bootstrap or SemanticUI is available */}
-                    <i className="fas fa-pen" style={actionBtnStyle}><sup></sup></i>
+                </Col>
+                <Col className="text-right">
+                    <Button variant="outline-dark"><i className="fas fa-pen" ></i></Button>
                     <Link to={`/product/${id}`}>
-                        <i className="fas fa-thumbs-up" style={actionBtnStyle}><sup>{likeCount}</sup></i>                    
+                        <Button variant="outline-dark" style={{paddingLeft:'0.55em', paddingRight:'0.2em'}}>
+                            <i className="fas fa-thumbs-up"></i>
+                            <sup><Badge variant="light">{likeCount}</Badge></sup>
+                        </Button>
                     </Link>
-                    <i className="fas fa-trash-alt" style={actionBtnStyle}><sup></sup></i>
-                </span>
-            </div>
-        </div>        
+                    <Button variant="outline-dark"><i className="fas fa-trash-alt" ></i></Button>
+                </Col>
+            </Row>
+        </Card>        
     )
 }
-
-//ToDo:: use Bootstrap or SemanticUI instead when available
-const boxStyle = {
-    color: "black",
-    backgroundColor: "white",
-    padding: "0.3rem 0.5rem",
-    margin: "0.3rem",
-    fontFamily: "Arial",
-    border: 'solid 1px lightgrey'
-  };
-
-const captionStyle = {
-    textAlign: 'left',
-    fontSize: '1.5rem'
-}
-
-const contentStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    fontSize: '1rem',
-    paddingTop: '0.5rem'
-}
-
-const actionBtnStyle = {
-    border: '1px solid black',
-    padding: '0.1em',
-    cursor: "pointer",
-    margin: "0 0.1em"
-}
-
 
 export default ListItem
--- src/Components/Home/ProductList.js
+++ src/Components/Home/ProductList.js
@@ -1,54 +1,37 @@
 import React from 'react'
 import ListItem from './ListItem'
 import { Link } from 'react-router-dom'
+import { Card, Col, Row, Button, Badge } from 'react-bootstrap'
 
 function ProductList(props) {
     return (
-        <div style={containerStyle}>
-            <div>
-                <div style={captionStyle}>Buyer's regret <i className="far fa-user-circle" style={userIconStyle}></i></div>
-                <div style={captionStyle}>My wishlist</div>
-            </div>
+        <div>
+            <Card className='p-2'>
+                <Card.Title className="text-center"> 
+                    <Row>
+                        <Col sm={{span:2}}></Col>
+                        <Col sm={{span:8}}>
+                            Buyer's regret
+                        </Col>
+                         <Col sm={{span:2}} style={{fontSize:"2em"}}><i className="far fa-user-circle align-middle"></i></Col> 
+                    </Row>
+                    <Row>
+                    <Col sm={{span:2}}></Col>
+                    <Col sm={{span:8}}>
+                            My wishlist
+                    </Col>
+                    <Col sm={{span:2}}></Col>
+                    </Row>
+                </Card.Title>
+            </Card>
             {props.allProducts.map((product) => (
                 <ListItem key={product.id} product={product}/>
             ))}
             <Link to='/product/add'>
-                <i className="fas fa-plus" style={actionBtnStyle}></i>
-            </Link>            
+                <Button variant="outline-dark" className="float-right my-3"><i className="fas fa-plus" ></i></Button>
+            </Link>          
         </div>
     )
 }
-
-//ToDo:: use framework for styles
-const containerStyle = {
-    minWidth:'15rem',
-    minHeight:'20rem', 
-    backgroundColor:"white",
-    position: 'relative',
-    color: 'black',
-}
-
-const captionStyle ={
-    border: '1px solid black',
-    padding: '0.3rem 0',
-    textAlign: 'center'
-}
-
-const actionBtnStyle = {
-    border: '1px solid black',
-    padding: '0.1rem',
-    cursor: "pointer",
-    margin: "0.1rem 0.1rem",
-    fontSize: '2rem',
-    position: 'absolute',
-    right: '0',
-    bottom:'0'
-}
-
-const userIconStyle = {
-    float:'right', 
-    padding: '0.5rem 0.5rem 0.5rem 0'
-}
-
 
 export default ProductList

c2ae5d8dd62eca40dedb01a62ea4e0390c582ec7
Dmytro Kulak <mail@dmytrokulak.net>
2021-01-28 21:12:14 +0200

    revert react-router usage

--- src/Components/Home/ListItem.js
+++ src/Components/Home/ListItem.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Card, Col, Row, Button, Badge } from 'react-bootstrap'
 
 function ListItem(prop) {
@@ -16,12 +15,10 @@ function ListItem(prop) {
                 </Col>
                 <Col className="text-right">
                     <Button variant="outline-dark"><i className="fas fa-pen" ></i></Button>
-                    <Link to={`/product/${id}`}>
-                        <Button variant="outline-dark" style={{paddingLeft:'0.55em', paddingRight:'0.2em'}}>
-                            <i className="fas fa-thumbs-up"></i>
-                            <sup><Badge variant="light">{likeCount}</Badge></sup>
-                        </Button>
-                    </Link>
+                    <Button variant="outline-dark" style={{paddingLeft:'0.55em', paddingRight:'0.2em'}}>
+                        <i className="fas fa-thumbs-up"></i>
+                        <sup><Badge variant="light">{likeCount}</Badge></sup>
+                    </Button>
                     <Button variant="outline-dark"><i className="fas fa-trash-alt" ></i></Button>
                 </Col>
             </Row>

2f57493a8e78c5f8c0aa97119ff43861108de6fb
Matthew Maggiolini <Magg01@gmail.com>
2021-02-01 20:36:27 +0100

    div to React.Fragment in PageSelection component

--- src/Components/PageSelection/PageSelection.js
+++ src/Components/PageSelection/PageSelection.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import React,{Fragment} from 'react';
 import Home from '../Home'
 import EditProduct from '../EditProduct'
 
@@ -6,10 +7,10 @@ const PageSelection = (props) => {
     const [selectedPage, setSelectedPage] = useState(props.pages.HOME)
 
     return (
-        <div>
+        <React.Fragment>
             {selectedPage === props.pages.HOME && <Home allProducts={props.allProducts} pages={props.pages} changePage={setSelectedPage} /> }
             {selectedPage === props.pages.EDIT_PRODUCT && <EditProduct pages={props.pages} changePage={setSelectedPage}/> }
-        </div>
+        </React.Fragment>
     )
 }
 

ccf3ebf1bb2a37d4a3d208e25fb2b36c603af63c
Matthew Maggiolini <Magg01@gmail.com>
2021-02-02 10:45:47 +0100

    comments for PageSelection & Pages

--- src/Components/PageSelection/PageSelection.js
+++ src/Components/PageSelection/PageSelection.js
@@ -2,8 +2,14 @@ import { useState } from 'react'
 import React,{Fragment} from 'react';
 import Home from '../Home'
 import EditProduct from '../EditProduct'
+//list of pages in the app
 import Pages from './Pages'
 
+
+// Componet used to track the currently selected page and switch between pages.
+// To change to a new page from another component import 'Pages' as above and
+// pass setSelectedPage method down the tree to your component. 
+// Call setSelectedPage(Pages.<new_page>) to change to the new_page page.
 const PageSelection = (props) => {
     const [selectedPage, setSelectedPage] = useState(Pages.HOME)
 

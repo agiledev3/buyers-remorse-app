da35d782cb472e98bfd0f860ef060bfec78ae996
Bruno de Rezende <brunoderezende@hotmail.com>
2021-03-07 09:33:02 -0300

    Adds comments to local storage test

--- src/Products/sources/LocalStorageSource.test.js
+++ src/Products/sources/LocalStorageSource.test.js
@@ -1,14 +1,18 @@
 import LocalStorageSource from "./LocalStorageSource";
 import testData from "./test-data.json";
 
+// This unit test checks if LocalStorageSource provides products data as expected
 describe("Products | LocalStorageSource", () => {
   describe(".read", () => {
     test("if it reads all products from test source", () => {
+      // Initialize local storage source with some dummy data
       const localStorageSource = LocalStorageSource.initialize(
         "products",
         JSON.stringify([...testData.products])
       );
+      // Read products from local storage
       const products = localStorageSource.read("products");
+      // Asserts if the returned data matches the dummy data
       expect(products).toEqual(testData.products);
     });
   });

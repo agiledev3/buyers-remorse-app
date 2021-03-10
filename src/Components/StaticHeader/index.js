c29604056bcb53370a0d1118a89d0d444f5618d6
Dmytro Kulak <mail@dmytrokulak.net>
2021-03-10 07:16:04 +0200

    Back button: fix markup alignment; extend clicking area

--- src/Components/StaticHeader/index.js
+++ src/Components/StaticHeader/index.js
@@ -7,13 +7,13 @@ function StaticHeader(props) {
     <Card className="my-2">
       <Card.Title className="text-center">
         <Row className="p-1">
-          <Col xs={{ span: 2 }} style={{ fontSize: "2em" }}>
+          <Col
+            xs={{ span: 2 }}
+            style={{ fontSize: "2em", marginBottom: "-1em" }}
+            onClick={onBackButtonClick ? () => onBackButtonClick() : () => {}}
+          >
             {onBackButtonClick && (
-              <Button
-                variant="outline-dark"
-                size="lg"
-                onClick={onBackButtonClick}
-              >
+              <Button variant="outline-dark" size="lg">
                 <i className="fas fa-chevron-left align-center"></i>
               </Button>
             )}
